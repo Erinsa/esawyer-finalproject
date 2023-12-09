cfd72a76596f4cee799801560da8a497b98be926
Erinsa <143417090+Erinsa@users.noreply.github.com>
2023-12-08 21:52:26 -0500

    more typos

--- src/views/home/menu.js
+++ src/views/home/menu.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import { Link, Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import GamePage from './index.js';
 // import { useState } from 'react';
 // import { AttentionSeeker } from "react-awesome-reveal";
